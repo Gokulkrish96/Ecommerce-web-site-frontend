37d8b591dbcb39d30f74a018e49f8d0971f77d6c
Gokulkrish96 <142777130+Gokulkrish96@users.noreply.github.com>
2023-10-29 10:21:43 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ import ReviewList from "./Components/admin/ReviewList";
 
 
 function App() {
+  const b = 5;
   const [stripeApiKey, setStripeApiKey] = useState("");
   useEffect(() => {
      store.dispatch(loadUser);
