39d1f81372b0966508ca51ac4d366b6db53ccc32
Gokulkrish96 <gokulst96@gmail.com>
2024-01-21 02:47:01 -0600

    12

--- src/Components/layout/Footer.js
+++ src/Components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="py-1">
-      <p className="text-center text-dark mt-1">
+      <p className="text-center text-dark mt-1 ">
         A2Z Shoppy- 2023-2024, All Rights Reserved
       </p>
     </footer>
